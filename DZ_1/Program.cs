948c2306c4b1e20c74e22b1f942543b82eb13112
Nikolay <kolune4ka@mail.ru>
2023-03-02 23:57:40 +0300

    Add HW6

---
+++ DZ_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задание. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArray(int arraySize)
+{
+    int[] array = new int[arraySize];
+    Console.WriteLine();
+    return array;
+}
+
+int[] ArrayFilling(int[] emptyArray)
+{
+    Console.WriteLine("Поочерёдно вводите любые положительные или отрицательные числа: ");
+    for(int i = 0; i < emptyArray.Length; i++) emptyArray[i] = int.Parse(Console.ReadLine()!);
+    return emptyArray;
+}
+
+void ResultOutput(int[] filledArray)
+{
+    int count = 0;
+    for(int i = 0; i < filledArray.Length; i++) if(filledArray[i] > 0) count += 1;
+    Console.WriteLine($"Чисел больше 0 было введено: {count}.");
+}
+
+Console.Clear();
+
+Console.Write("Ввведите количество чисел (M), которые вы будете затем вводить: ");
+int M = int.Parse(Console.ReadLine()!);
+
+ResultOutput(ArrayFilling(CreateArray(M)));
---
+++ DZ_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.
+// Значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void FindPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    if(k1 == k2 && b1 != b2) Console.WriteLine("Прямые не пересекаются");
+    else if(k1 == k2 &&  b1 == b2) Console.WriteLine("Пряме совпадают");
+    else Console.WriteLine($"Координаты точки пересечения: {x}; {y}.");
+}
+
+Console.Clear();
+
+Console.WriteLine("Найти точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.");
+Console.WriteLine();
+
+Console.Write("Введите значение b1: ");
+double B1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k1: ");
+double K1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+double B2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+double K2 = double.Parse(Console.ReadLine()!);
+
+FindPoint(B1, K1, B2, K2);
